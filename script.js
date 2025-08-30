73063f1cdf147f491db4d100a952e6f1f8551fb1
Surya <ksurya112003@gmail.com>
2025-08-30 20:29:04 +0530

    First Portfolio commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function topen(){
+    document.querySelector(".h-nav-data").classList.toggle("active");
+    document.querySelector(".ham-icon").classList.toggle("active");
+    document.querySelector(".close-icon").classList.toggle("active");
+}
